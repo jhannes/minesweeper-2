875c6e56c71c3fe893224e9869520d7e33f7c0f8
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-08-19 17:26:47 +0200

    Initial commit

---
+++ src/no/kristiania/minesweeper/MinesweeperTest.java
@@ -0,0 +1,19 @@
+package no.kristiania.minesweeper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class MinesweeperTest {
+    @Test
+    void itShouldEmptyMinefield() {
+        assertArrayEquals(
+                new String[] { "0" },
+                displayBoard(new String[] { "." })
+        );
+    }
+
+    private String[] displayBoard(String[] input) {
+        return new String[0];
+    }
+}
